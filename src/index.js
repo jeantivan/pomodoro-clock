7c816f62d108b709fd7216a2a09876e17d48c853
Jean Paul Tivan <jptivan53@gmail.com>
2022-05-30 22:16:42 -0400

    Correcto Id de la App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./components";
 import "./index.scss";
 
 // Context
 import { PomodoroProvider } from "./Context";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("App"));
 root.render(
   <React.StrictMode>
     <PomodoroProvider>
